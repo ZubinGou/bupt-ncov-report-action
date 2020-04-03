8bbfd8e7b9cb0b0d306439f4daa7c68874240db0
ZubinGou <zebgou@gmail.com>
2020-04-03 11:46:57 +0800

    Debug GitHub Actions

--- src/index.ts
+++ src/index.ts
@@ -131,7 +131,8 @@ async function postDailyReportFormData(
     const chatId = process.env["TG_CHAT_ID"];
     const botToken = process.env["TG_BOT_TOKEN"];
 
-    if (!!chatId && !!botToken && reportReponse.m !== "今天已经填报了") {
+    // if (!!chatId && !!botToken && reportReponse.m !== "今天已经填报了") {
+    if (!!chatId && !!botToken) {
         const bot = new TelegramBot(botToken);
         await bot.sendMessage(
             chatId,
